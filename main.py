e9d401bae90e359333785e7560c3f0fc7f8fd667
fibersel <0583463@gmail.com>
2019-03-11 15:13:30 +0300

    added shutdown on button

--- main.py
+++ main.py
@@ -1,13 +1,26 @@
 from tkinter import *
+import time
+
+SHUTDOWN_CTR = 5
+
+k = 0
+
+
 
 def button_clicker():
+	global k
+	if SHUTDOWN_CTR <= k:
+		exit(13)
+	k += 1
+	button['text'] = 'clickme, TIME:' + time.strftime('%H:%M:%S')
 	print('кек')
 
 
 root = Tk()
-
-button1 = Button(root, bg='red', text='clickme', command=button_clicker)
-button1.pack()
+label = Label(text='kekovich')
+label.grid()
+button = Button(root, bg='red', text='clickme, TIME:' + time.strftime('%H:%M:%S'), command=button_clicker)
+button.grid()
 
 root.mainloop()
 
