3c27e27dadb7d724a23ef4bb7daaf08b322f03a5
fibersel <0583463@gmail.com>
2019-03-12 15:21:28 +0300

    more 2 buttons added

--- main.py
+++ main.py
@@ -18,9 +18,12 @@ def button_clicker():
 
 root = Tk()
 label = Label(text='kekovich')
-label.grid()
-button = Button(root, bg='red', text='clickme, TIME:' + time.strftime('%H:%M:%S'), command=button_clicker)
-button.grid()
-
+label.pack(side=TOP)
+button1 = Button(root, bg='red', text='clickme, TIME:' + time.strftime('%H:%M:%S'), command=button_clicker)
+button1.pack(side=LEFT)
+buttom2 = Button(root, bg='blue', text='clickme_2', command=lambda: print('have a nice day!'))
+buttom2.pack(side=LEFT)
+button3 = Button(root, bg='green', text='clickme_3', command=lambda: print('yet another command'))
+button3.pack(side=LEFT)
 root.mainloop()
 
